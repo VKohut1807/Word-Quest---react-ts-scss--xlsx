5ab549e39b6a555fb2e088e6cb6affcc1a3b2047
VKohut1807 <vasylkohut1991@gmail.com>
2025-03-23 20:58:45 +0100

    feat:F---up-button

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ROUTES } from "@/routes";
+import React, {useState, useEffect} from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {ROUTES} from "@/routes";
 
 import "@/assets/scss/config/index.scss";
 import "@/assets/scss/global/index.scss";
 
-import type { LocalStorage } from "@/types/dictionary-types";
+import type {LocalStorage} from "@/types/dictionary-types";
 
 import Home from "@/pages/Home";
 import FileUploader from "@/pages/FileUploader";
 import Dictionary from "@/pages/Dictionary";
 import Novbar from "@/components/Novbar";
+import UpButton from "@/components/UpButton";
 import RouteWrapper from "@/components/RouteWrapper";
 
-import { getItem } from "@/helpers/persistanceStorage";
+import {getItem} from "@/helpers/persistanceStorage";
 
 const App: React.FC = () => {
   const [excelData, setExcelData] = useState<LocalStorage[]>([]);
@@ -29,6 +30,7 @@ const App: React.FC = () => {
     <Router>
       <>
         <Novbar />
+        <UpButton />
 
         <Routes>
           <Route
