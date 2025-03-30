1559655690fd75838d3330fc933846c9cccedc7c
VKohut1807 <vasylkohut1991@gmail.com>
2025-03-30 15:58:37 +0200

    chore:F---test-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import {HashRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {ROUTES} from "@/routes";
 
 import "@/assets/scss/config/index.scss";
