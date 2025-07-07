cbf1b449c69e9af04f14eb6c7a940ef9799964bf
VKohut1807 <vasylkohut1991@gmail.com>
2025-07-07 23:22:12 +0200

    feat:F---added-back-slider-card-----test

--- src/components/FlipCard.tsx
+++ src/components/FlipCard.tsx
@@ -5,12 +5,13 @@ import "@/assets/scss/components/flip-card.scss";
 import type {FlipCardProps} from "@/types";
 
 const FlipCard: React.FC<FlipCardProps> = ({className = "", front, back}) => {
-    const [flipCard, setFlipCard] = useState<boolean>(true);
+    const [flipCard, setFlipCard] = useState<boolean>(false);
 
     return (
         <section
             onClick={() => setFlipCard((prev) => !prev)}
-            className={`flip-card-box ${flipCard ? "" : "active"} ${className}`}
+            onTouchStart={() => setFlipCard((prev) => !prev)}
+            className={`flip-card-box ${flipCard ? "active" : ""} ${className}`}
         >
             <div className="card-front">{front}</div>
             <div className="card-back">{back}</div>
